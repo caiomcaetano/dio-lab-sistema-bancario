f9453e66f69c3ce4ea5f609831d02bcedb72f2fe
caiomcaetano <caio_caetano@live.com>
2023-08-08 23:13:15 -0300

    Versão 2

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -17,27 +17,36 @@ while True:
 
     if entrada == 1:
         valor_deposito = float(input('Digite o valor desejado: '))
-        if valor_deposito < 0:
-            print('Operação inválida!')
+        if valor_deposito <= 0:
+            print('O depósito deve ser maior que R$ 0,00.')
         else:
             saldo += valor_deposito
             extrato.append(f'Depósito: R$ {valor_deposito:.2f}')
 
     if entrada == 2:
         valor_saque = float(input('Digite o valor desejado: '))
-        if valor_saque < 0  or valor_saque > 500 or valor_saque > saldo or numero_saque >= 3:
-            print('Operação inválida!')
+        if valor_saque <= 0  or valor_saque > 500:
+            print('O saque deve ser maior que R$ 0,00 e no máximo até R$ 500,00.')
+        elif valor_saque > saldo:
+            print(f'Saldo indisponível! O seu saldo atual é de R$ {saldo:.2f}')
+        elif numero_saque >= 3:
+            print('Limite de saques diário ultrapassado!')
         else:
             saldo -= valor_saque
             numero_saque += 1
             extrato.append(f'Saque: R$ {valor_saque:.2f}')
     
     if entrada == 3:
-        print('---------------Extrato--------------')
-        for i in range(0, len(extrato)):
-            print(extrato[i])
-        print(f'\nSaldo: R$ {saldo:.2f}')
-        print('------------------------------------')
+        if not extrato:
+            print('---------------Extrato--------------')
+            print('\nNão foram realizadas movimentações!')
+            print('------------------------------------')
+        else:
+            print('---------------Extrato--------------')
+            for i in range(0, len(extrato)):
+                print(extrato[i])
+            print(f'\nSaldo: R$ {saldo:.2f}')
+            print('------------------------------------')
     
     if entrada == 4:
         break
